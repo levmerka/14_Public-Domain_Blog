51e5117989afb8ecfd682735da2c205bf6d336a7
Mitchell Lev Merka <lev@merka.us>
2022-09-03 16:46:34 -0700

    added seeds, login route fixed

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const userRouter = require('./user-route');
 const postRouter = require('./post-route');
 const commentRouter = require('./comment-route');
-
+const homeRouter =require('./home-route')
 router.get('/', (req,res) => {
   res.render('homepage', {
     postData: "lobster"
   });
 })
 
+router.use('/', homeRouter);
 router.use('/api/user', userRouter)
+// router.use('/posts', postRouter)
 module.exports = router
